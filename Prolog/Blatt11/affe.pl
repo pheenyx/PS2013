ea1bf2d48d41625d09b42f3ae30c4dbc40698c1d
Michael Mardaus <mardaus@students.uni-mainz.de>
2013-06-19 18:17:42 +0200

    prolog affe

---
+++ Prolog/Blatt11/affe.pl
@@ -0,0 +1,18 @@
+move(state(middle, on_box, middle, has_not),
+    grasp,
+    state(middle, on_box, middle, has)).
+
+move(state(P, on_floor, P, H),
+    climb,
+    state(P, on_box, P, H)  ).
+
+move(state(P1, on_floor, P1, H),
+    push(P1, P2),
+    state(P2, on_floor, P2, H)).
+
+move(state(P1, on_floor, P, H),
+    walk(P1, P2),
+    state(P2, on_floor, P, H)).
+
+actions(state(_,_,_,has),[]).
+actions(S,[M|L]):-move(S,M,S1),actions(S1, L).
